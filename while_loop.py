c583e374e282248dfac7d2f27b45c50fd367dae1
mugilanaaru <mugil.aaru@gmail.com>
2025-04-05 23:15:10 +0530

    loop file

---
+++ while_loop.py
@@ -0,0 +1,34 @@
+##########################  WHILE LOOP ##################################
+#The for loop is usually used when the number of iterations is known
+#The while loop is usually used when number of iterations is unknown
+"""
+i=1
+while(i<5):
+    print(i)
+    i=i+1
+"""
+##############################
+"""
+i=10
+while(i<=200):
+    print(i,end=",")
+    i=i+10
+"""
+#########################################
+##### To print 1 to 10 in reverse order
+
+i=10
+while(i>0):
+    print(i,end=",")
+    i=i-1
+
+######################################
+"""
+#### To print factorial of number
+i=3
+fact=1
+while(i>0):
+    fact=fact*i
+    i=i-1
+print(fact)
+"""
