8abf54aeddfd856abd82ce416fdc09d87c2214b8
mugilanaaru <mugil.aaru@gmail.com>
2025-06-16 00:34:52 +0530

    adding class

---
+++ class_and_object.py
@@ -0,0 +1,27 @@
+class laptop:
+    price=""
+    Processor=""
+    Ram=""
+
+HP = laptop()
+DELL = laptop()
+LENEVO = laptop()
+
+HP.price=21000
+DELL.price=22000
+LENEVO.price=24000
+
+HP.Processor="i5"
+DELL.Processor="i5"
+LENEVO.Processor="i5"
+
+HP.Ram="4GB"
+DELL.Ram="8GB"
+LENEVO.Ram="8GB"
+
+
+#a=int(input("Enter the model : "))
+#b=int(input("Enter the config : "))
+print(HP.Ram)
+
+
