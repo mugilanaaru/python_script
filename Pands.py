e614d4ce9c7d66455a7542d2def8e6a700b66197
mugilanaaru <mugil.aaru@gmail.com>
2026-03-26 08:46:39 +0530

    updating pandas file

--- Pands.py
+++ Pands.py
@@ -13,3 +13,19 @@ print(f"print scores greater than 70 : \n{filtered}.sort_values()")
 #print(f"print the scores greater than 70 : {scores[scores > 70]}")
 
 print(scores['Science'])
+
+############## Dictinory data ###############
+
+data = {'Monday': 100, 'Tuesday': 200, 'Wednesday': 300}   ### dict structure
+sales = pa.Series(data)      ###### converting dict to series
+print(f"converting dict to series : \n{sales}")
+
+#data frame
+
+data1 = {
+    'Name': ['Mugil', 'Tamil', 'Sanjeev', 'Agil'],
+    'Age': [35, 30, 8, 3],
+    'City': ['Chennai', 'Delhi', 'Munbai', 'kolkata'],
+}
+df = pa.DataFrame(data1)
+print(df)
