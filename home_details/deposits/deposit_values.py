fece358fb02e34523e8c764170050af8de1b205d
mugilanaaru <mugil.aaru@gmail.com>
2026-04-14 20:40:33 +0530

    updating deposit folder

--- home_details/deposits/deposit_values.py
+++ home_details/deposits/deposit_values.py
@@ -8,35 +8,48 @@ con = mysql.connector.connect(**config)
 
 
 def select1():
-    print("To print the details of all accounts owned by a person - enter 'Name' : ")
-    print("To print the details of a account - enter 'AC_No :  ")
-    print("To print the details a account by paticular period - Enter 'period' : ")
-    print("To print the details a account by paticular amount - Enter 'Principal_Amount' ")
-    print("To print the details of a account by date - Enter 'effect_from_date'")
-    print("To filter a account by its maturiy date - Enter 'maturity_date'")
-    print("To filter a account by its maturiy account - Enter 'Maturity_Amount'")
-    print("To filter a account by its Interest rate - Enter 'Interest'")
+    print("\n To view a particular person's deposits - press '1' : \n")
+    print("To view or filter a particular Account number - press '2'  :  \n")
+    print("To view deposits by a particular period - press '3' : \n")
+    print("To filter the details by paticular amount - press '4'  \n")
+    print("To filter the details by a particular initial date - press '5' \n")
+    print("To filter the details by a particular maturity date - press '6' \n")
+    print("To filter the details by paticular maturity amount - press '7' \n")
+    print("To filter the details by particular rate of interest - press '8' \n")
+    print("To filter the details by a particular bank - press '9' \n")
     option = input("\nwhich one you want to view : ")
-    if option == 'Name':
+    if option == '1':
+       options = "Name"
        Name= input("Enter the Name: ")
-    elif option == 'AC_No':
+    elif option == '2':
+       options = "AC_No"
        Name = input("Enter the account number: ")
-    elif option == "period":
+    elif option == "3":
+       options = "period"
        Name = input("Enter the period duration")
-    elif option == "Principal_Amount":
+    elif option == "4":
+       options = "Principal_Amount"
        Name = input("Enter the amount to check")
-    elif option == "effect_from_date":
+    elif option == "5":
+       options = "effect_from_date"
        Name = input("Enter the date to check format should be 2025-11-10: ")
-    elif option == "maturity_date":
+    elif option == "6":
+       options = "maturity_date"
        Name = input("Enter the maturity date to check format should be 2025-11-10: ")
-    elif option == "Maturity_Amount":
+    elif option == "7":
+       options = "Maturity_Amount"
        Name = input("Enter the amount to show : ")
-    elif option == "Interest":
-       Name = input("Enter the number to check : ")
+    elif option == "8":
+       options = "Interest"
+       Name = input("Enter the interest to check : ")
+    elif option == "9":
+       options = "Bank_Name"
+       Name = input("Enter the bank name to check : ")
     res=con.cursor()
-    sql = f"select * from deposits where {option} like %s;"
+    sql = f"select * from deposits where {options} like %s;"
     user=(Name)
     res.execute(sql,("%" + user + "%",))
     result=res.fetchall()
-    print(tabulate(result,headers=["ID","Name","Account Number","period","Principal_Amount","effect_from_date","maturity_date","Maturity_Amount","Interest %"]))
+    print("\n")
+    print(tabulate(result,headers=["ID","Name","Ac No","period","Amount","Start Date","Maturity Date","Maturity Amount","Interest","Bank"]))
     print("\n\n")
